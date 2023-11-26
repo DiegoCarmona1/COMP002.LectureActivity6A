6334615504ac8062037545d0adcf4026d11f8071
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-25 23:45:36 -0800

    added try and catch block

--- script.js
+++ script.js
@@ -14,7 +14,15 @@ function except(myNumber){
     if (isNaN(myNumber)){
         throw new Error ("Invalid Input, Must Enter Numbers")
     }
-
+    while (true) {
+        let myNumber = promptForNumber ();
+        try {
+            let result = except(myNumber);
+        }
+        catch (error) {
+            console.log("Please Enter Only Numeric Values!");
+        }
+    }
     return myNumber
 }
 console.log("My favorite number is " + except(6))
