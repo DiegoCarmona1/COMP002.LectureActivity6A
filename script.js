4f1a62360e0a752068e75ff6fd77211887fe11d7
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-25 23:40:47 -0800

    added function with exception

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ for (let i = 0; i < myGrades.length; i++) {
 myAverage = myAverage / myGrades.length
 
 console.log("My grade average is " + myAverage);
+
+function except(myNumber){
+    if (isNaN(myNumber)){
+        throw new Error ("Invalid Input, Must Enter Numbers")
+    }
+
+    return myNumber
+}
+console.log("My favorite number is " + except(6))
